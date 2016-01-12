27ed0411e5d4697a47a16f1ea519f4e11f171428
Jasonface900 <jorge.floresm900@gmail.com>
2016-01-12 16:53:50 -0600

    updated code, swiched syntax

--- SampleBot Code Mk1.java
+++ SampleBot Code Mk1.java
@@ -1,19 +1,29 @@
 public class SampleBot extends SampleRobot{
     
     Victor MotorA;
+    Joystick Joy1;
     
-    public void RobotInit(){
+    public void robotInit(){
         MotorA = new Victor(0);
+        Joy1 = new Joystick(0);
     }
     
-    public void AutonomousPeriodic(){
+    public void autonomousPeriodic(){
         MotorA.set(0.5);
-        wait(1);
+        Timer.Delay(1);
         MotorA.set(0.0);
-        wait(.5);
+        Timer.Delay(.5);
         MotorA.set(-0.5);
-        wait(1);
+        Timer.Delay(1);
         MotorA.set(0.0);
+    }
+    
+    public void operatorControl() {
+    	while(isOperatorControl && isEnabled) {
+    		MotorA.set(Joy1.GetX());
+    		Timer.Delay(0.05); //motor update time
+    	}
+    	
     }
     
 }
