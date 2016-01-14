7e8743906897359069ac6dd8b80369d73c2a6486
Jasonface900 <jorge.floresm900@gmail.com>
2016-01-13 18:36:57 -0600

    updated the code, added import

--- SampleBot Code Mk1.java
+++ SampleBot Code Mk1.java
@@ -1,3 +1,9 @@
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.livewindow.Livewindow;
+
 public class SampleBot extends SampleRobot{
     
     Victor MotorA;
